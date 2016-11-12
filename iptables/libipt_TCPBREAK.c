e6622757fa68fa31f646706cdfae9e48257d21c9
Vitaly Lavrov <lve@guap.ru>
2016-11-12 23:49:12 +0300

    Added encode/decode string ( \r \n \t \xx  )

--- iptables/libipt_TCPBREAK.c
+++ iptables/libipt_TCPBREAK.c
@@ -27,6 +27,90 @@ static const struct xt_option_entry tcpbreak_tg_opts[] = {
 };
 #undef s
 
+static char _print_buf[sizeof(struct xt_tcpbreak_tgt)*2];
+static char *cvt_hex = "0123456789abcdef";
+
+static char *printable(const char *src) {
+char c,*o = _print_buf;
+int len = sizeof(_print_buf)-1;
+while(len && !!(c = *src++)) {
+	if (c >= ' ' && c != '\\' && (unsigned char)c < 127 ) {
+		*o++ = c, len--;
+		continue;
+	}
+	/* skip utf-8 code */
+	if((c & 0xe0) == 0xc0 && (*src & 0xc0) == 0x80) {
+		*o++ = c, len--;
+		if(!len) break;
+		*o++ = *src++, len--;
+		continue;
+	}
+	*o++ = '\\', len--;
+	if(!len) break;
+	if (c == '\\') {
+		*o++ = c, len--;
+	} else
+	if (c == '\r') {
+		*o++ = 'r', len--;
+	} else
+	if (c == '\n') {
+		*o++ = 'n', len--;
+	} else
+	if (c == '\t') {
+		*o++ = 't', len--;
+	} else {
+		if(len >= 2) {
+    		*o++ = cvt_hex[(c >> 4) & 0xf];
+    		*o++ = cvt_hex[c & 0xf];
+		len -= 2;
+		} else break;
+	}
+}
+*o = '\0';
+return _print_buf;
+}
+
+static char *parse_opt_string(const char *src,char *buf, size_t len) {
+char c;
+int r;
+
+while(len > 0 && !!(c = *src++)) {
+    if(c == '\\') {
+	c = *src++;
+	switch(c) {
+	  case '\\':
+		  break;
+	  case 'r':
+		  c = '\r';
+		  break;
+	  case 'n':
+		  c = '\n';
+		  break;
+	  case 't':
+		  c = '\t';
+		  break;
+	  default:
+		  if (strchr(cvt_hex, c) && strchr(cvt_hex, *src)) {
+			r = (strchr(cvt_hex, c) - &cvt_hex[0]) << 4;
+			c = *src++;
+			r |= strchr(cvt_hex, c) - &cvt_hex[0];
+			c = r;
+		  } else {
+		  	*buf++ = '\\'; len--;
+		  }
+		  break;
+	}
+    }
+    if(!c) break;
+    if(len) {
+	*buf++ = c;
+	len--;
+    }
+}
+*buf = '\0';
+return buf;
+}
+
 static void tcpbreak_tg_help(void)
 {
 	printf("tcpbreak [--http302 url | --raw string]\n\n");
@@ -37,14 +121,14 @@ static void tcpbreak_tg_save(const void *ip, const struct xt_entry_target *targe
 	struct xt_tcpbreak_tgt *tginfo = (void *)target->data;
 	if(tginfo->mode)
 		printf(" --%s %s",tginfo->mode == 'L' ? "http302":"raw",
-				  tginfo->location);
+				  printable(tginfo->location));
 }
 static void tcpbreak_tg_print(const void *ip, const struct xt_entry_target *target,
 		                          int numeric)
 {
 	struct xt_tcpbreak_tgt *tginfo = (void *)target->data;
 	if(tginfo->mode)
-		printf(" %s %s", tginfo->mode == 'L' ? "http302":"raw", tginfo->location);
+		printf(" %s '%s'", tginfo->mode == 'L' ? "http302":"raw", printable(tginfo->location));
 }
 
 static void tcpbreak_tg_parse(struct xt_option_call *cb)
@@ -56,14 +140,14 @@ static void tcpbreak_tg_parse(struct xt_option_call *cb)
 		if(tginfo->mode == 'R')
 	                xtables_error(PARAMETER_PROBLEM,
         	                   "TCPBREAK target: can't mix http302 and raw");
-		strncpy(tginfo->location,cb->arg,sizeof(tginfo->location)-1);
+		parse_opt_string(cb->arg,tginfo->location,sizeof(tginfo->location)-1);
 		tginfo->mode = 'L';
 		break;
 	case O_SET_RAW:
 		if(tginfo->mode == 'L')
 	                xtables_error(PARAMETER_PROBLEM,
         	                   "TCPBREAK target: can't mix http302 and raw");
-		strncpy(tginfo->location,cb->arg,sizeof(tginfo->location)-1);
+		parse_opt_string(cb->arg,tginfo->location,sizeof(tginfo->location)-1);
 		tginfo->mode = 'R';
 		break;
 	default:
